d682187c39f3d65a30d28b666bec4ef4dff0f53a
Melik B <melikbalat12@gmail.com>
2025-10-30 15:07:00 -0400

    Update app.py

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -9,10 +9,17 @@ def home():
 
 @app.route('/api/crime')
 def crime_data():
-    url = "https://phl.carto.com/api/v2/sql?q=SELECT%20dispatch_date_time,%20text_general_code,%20location_block%20FROM%20incidents_part1_part2%20LIMIT%2025"
+    url = (
+        "https://phl.carto.com/api/v2/sql?"
+        "q=SELECT dispatch_date_time, text_general_code, location_block "
+        "FROM incidents_part1_part2 "
+        "ORDER BY dispatch_date_time DESC "
+        "LIMIT 50"
+    )
     response = requests.get(url).json()
     crimes = response['rows']
     return crimes
+
 
 if __name__ == "__main__":
     app.run(debug=True)
